56fe4fd81aa0af33b95ea046a4d849f417d6918d
Ken Ilo <keneilo91@yahoo.com>
2022-02-06 17:07:28 -0600

    added about page in nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import "./App.css";
@@ -7,11 +8,10 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
-
-
 function App() {
   return (
     <div className="App">
+       <Header />
       <Footer/>
     </div>
   );
