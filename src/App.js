365211b442c0fb64006ff4f026ed76ae4fe424e3
Ken Ilo <keneilo91@yahoo.com>
2022-02-06 20:34:31 -0600

    added footer and minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-//import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
        <Header />
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   );
 }
